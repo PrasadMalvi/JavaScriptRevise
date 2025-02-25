eefe5495ce07d925a80ef18bbff8f29e3df31b19
Prasad Malvi <prasadmalvi23@gmail.com>
2025-02-25 21:26:14 +0530

    Factorial Program

---
+++ BasicES6/Factorial.js
@@ -0,0 +1,25 @@
+let fact = 1;
+for (let i = 1; i <= 5; i++) {
+  fact = fact * i;
+}
+
+console.log(fact);
+
+console.log("Prasad");
+console.log("Prasad");
+console.log("Prasad");
+console.log("Prasad");
+console.log("Prasad");
+
+console.log("\n");
+
+for (let i = 0; i < 5; i++) {
+  console.log("Prasad");
+}
+
+let arr = [1, 2, 3, 4, 5, 6];
+console.log(arr[0]);
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
