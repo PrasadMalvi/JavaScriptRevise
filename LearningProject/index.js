59d7874be5351a620869826bbae9e7e8c4eecb84
Prasad Malvi <prasadmalvi23@gmail.com>
2025-03-07 22:08:04 +0530

    Create api for frontend and back end how to diffenciate

--- LearningProject/index.js
+++ LearningProject/index.js
@@ -6,7 +6,22 @@ const PORT = 8000;
 
 //Routes
 app.get("/users", (req, res) => {
+  const html = `
+  <ul>
+  ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+  </ul>
+  `;
+  res.send(html);
+});
+
+app.get("/api/users", (req, res) => {
   return res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
 });
 
 app.listen(PORT, () => console.log(`Server runnning on Port ${PORT}`));
