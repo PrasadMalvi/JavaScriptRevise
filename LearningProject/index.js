3cfd9eccddd3b66ccea30e4dbec5b570654eaadd
Prasad Malvi <prasadmalvi23@gmail.com>
2025-03-07 22:17:20 +0530

    Created and used same get api Method for multiple API request

--- LearningProject/index.js
+++ LearningProject/index.js
@@ -18,10 +18,23 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-app.get("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const user = users.find((user) => user.id === id);
-  return res.json(user);
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    return res.json({ status: "Pending" });
+  })
+  .delete((req, res) => {
+    return res.json({ status: "Pending" });
+  });
+
+app.post("/api/users", (req, res) => {
+  //TODO : Create NewUser
+  return res.json({ status: "Pending" });
 });
 
 app.listen(PORT, () => console.log(`Server runnning on Port ${PORT}`));
